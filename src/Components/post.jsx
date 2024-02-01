89fe7df94a27e1adea2a7ec1ac7109e49d90182c
GabrielManojo <gabriel.manojo@gmail.com>
2024-01-31 19:43:30 -0800

    I finish the code

--- src/Components/post.jsx
+++ src/Components/post.jsx
@@ -1,7 +1,17 @@
+import style from './Post.module.css';
+
 export function Post(){
     return(
-        <div>
-            
-        </div>
+        <article className={style.Post}>
+            <header>
+                <div className={styles.author}>
+                    <img  className={styles.avatar} src="https://github.com/GabrielManojo.png" />
+                    <div>
+                        <strong>Gabriel Manojo</strong>
+                        <span> Web developer</span>
+                    </div>
+                </div>
+            </header>
+        </article>
     )
 }
